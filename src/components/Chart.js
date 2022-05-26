ef202d9983f7589c4e3d3e9136666acd4c607513
Rashmi Mhatre <rasmhatr@WKMZTDFA4678.global.publicisgroupe.net>
2022-05-26 11:33:44 +0530

    removing warnings

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -14,23 +14,6 @@ import {
 import Title from "./Title";
 import moment from "moment";
 
-// Generate Sales Data
-function createData(time, amount) {
-  return { time, amount };
-}
-
-const data = [
-  createData("00:00", 0),
-  createData("03:00", 300),
-  createData("06:00", 600),
-  createData("09:00", 800),
-  createData("12:00", 1500),
-  createData("15:00", 2000),
-  createData("18:00", 2400),
-  createData("21:00", 2400),
-  createData("24:00", undefined),
-];
-
 export default function Chart({ rowData, date }) {
   const theme = useTheme();
   return (
